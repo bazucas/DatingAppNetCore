95f2432b7b8c52caa84ff10374cba41533d6c8a0
Luís Ricardo Silva Inácio <luis.inacio@aitec.pt>
2018-06-28 13:35:19 +0100

    AuthService map issue

--- SPA/src/app/_services/auth.service.ts
+++ SPA/src/app/_services/auth.service.ts
@@ -4,7 +4,7 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class AuthService {
-  baseUrl = 'http://localhost:5000/api/auth';
+  baseUrl = 'http://localhost:5000/api/auth/';
   userToken: any;
 
 constructor(private http: Http) { }
@@ -12,12 +12,12 @@ constructor(private http: Http) { }
   login(model: any) {
     const headers = new Headers({'Content-type': 'application/json'});
     const options = new RequestOptions({headers: headers});
-    return this.http.post(this.baseUrl + 'login', model, options).pipe(map((response: Response) => {
+    return this.http.post(this.baseUrl + 'login', model, options).map((response: Response) => {
       const user = response.json();
       if (user) {
         localStorage.setItem('token', user.tokenString);
         this.userToken = user.tokenString;
       }
-    }));
+    });
   }
 }
