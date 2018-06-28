e278fc9c76afccba0dbbfbb0e307ced7cf399ec9
Luís Ricardo Silva Inácio <luis.inacio@aitec.pt>
2018-06-28 13:19:50 +0100

    AuthService changed

--- SPA/src/app/_services/auth.service.ts
+++ SPA/src/app/_services/auth.service.ts
@@ -1,25 +1,23 @@
-import { Injectable } from '@angular/core';
 import { Http, RequestOptions, Headers, Response } from '@angular/http';
+import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class AuthService {
   baseUrl = 'http://localhost:5000/api/auth';
   userToken: any;
 
 constructor(private http: Http) { }
 
-login(model: any) {
-  const headers = new Headers({'Content-type': 'application/json'});
-  const options = new RequestOptions({headers: headers});
-  return this.http.post(this.baseUrl + 'login', model, options).map((response: Response) => {
-    const user = response.json();
-    if (user) {
-      localStorage.setItem('token', user.tokenString);
-      this.userToken = user.tokenString;
-    }
-  });
-}
+  login(model: any) {
+    const headers = new Headers({'Content-type': 'application/json'});
+    const options = new RequestOptions({headers: headers});
+    return this.http.post(this.baseUrl + 'login', model, options).pipe(map((response: Response) => {
+      const user = response.json();
+      if (user) {
+        localStorage.setItem('token', user.tokenString);
+        this.userToken = user.tokenString;
+      }
+    }));
+  }
 }
