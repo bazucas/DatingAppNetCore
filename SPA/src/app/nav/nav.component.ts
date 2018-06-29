2b4d61bcdcaba89fcce69ea23c9102611aef12ac
Luís Ricardo Silva Inácio <luis.inacio@aitec.pt>
2018-06-29 16:30:08 +0100

    Routing between components

--- SPA/src/app/nav/nav.component.ts
+++ SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { AlertifyService } from './../_services/alertify.service';
 import { AuthService } from './../_services/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -10,7 +11,9 @@ import { Component, OnInit } from '@angular/core';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(public authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService,
+              private alertify: AlertifyService,
+              private router: Router) { }
 
   ngOnInit() { }
 
@@ -18,7 +21,9 @@ export class NavComponent implements OnInit {
     this.authService.login(this.model).subscribe(data => {
       this.alertify.success('Logged in successfully');
     }, error => {
-      this.alertify.error('Loggin error');
+      this.alertify.error('Failed to login');
+    }, () => {
+      this.router.navigate(['/members']);
     });
   }
 
@@ -26,6 +31,7 @@ export class NavComponent implements OnInit {
     this.authService.userToken = null;
     localStorage.removeItem('token');
     this.alertify.message('logged out');
+    this.router.navigate(['/home']);
   }
 
   loggedIn() {
