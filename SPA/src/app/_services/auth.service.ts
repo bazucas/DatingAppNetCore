5c0eb530f3c5c8c4fd3b38688b70677fc73ec617
Luís Ricardo Silva Inácio <luis.inacio@aitec.pt>
2018-06-29 11:48:50 +0100

    Error handling

--- SPA/src/app/_services/auth.service.ts
+++ SPA/src/app/_services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Http, RequestOptions, Headers, Response } from '@angular/http';
 import { Injectable } from '@angular/core';
-import { map } from 'rxjs/operators';
+import { map, catchError } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
 
 @Injectable()
 export class AuthService {
@@ -16,15 +17,34 @@ constructor(private http: Http) { }
         localStorage.setItem('token', user.tokenString);
         this.userToken = user.tokenString;
       }
-    }));
+    })).pipe(catchError(this.handleError));
   }
 
   register(model: any) {
-    return this.http.post(this.baseUrl + 'register', model, this.requestOptions());
+    return this.http.post(this.baseUrl + 'register', model, this.requestOptions()).pipe(catchError(this.handleError));
   }
 
   private requestOptions() {
     const headers = new Headers({'Content-type': 'application/json'});
     return new RequestOptions({headers: headers});
+  }
+
+  private handleError(error: any) {
+    const applicationError = error.headers.get('Application-Error');
+    if (applicationError) {
+      return Observable.throw(applicationError);
+    }
+    const serverError = error.json();
+    let modelStateErrors = '';
+    if (serverError) {
+      for (const key in serverError) {
+        if (serverError[key]) {
+          modelStateErrors += serverError[key] + '\n';
+        }
+      }
+    }
+    return throwError(
+      modelStateErrors || 'Server error'
+    );
   }
 }
