43175e92ba56203682f4c20ac1ac6ccac217f63d
Luís Ricardo Silva Inácio <luis.inacio@aitec.pt>
2018-07-04 16:49:58 +0100

    Server side Pagination done

--- API/Data/IDatingRepository.cs
+++ API/Data/IDatingRepository.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using API.Helpers;
 using API.Models;
 
 namespace API.Data
@@ -10,7 +11,7 @@ namespace API.Data
         void Delete<T>(T entity) where T: class;
         Task<bool> SaveAll();
 
-        Task<IEnumerable<User>> GetUsers();
+        Task<PagedList<User>> GetUsers(UserParams userParams);
 
         Task<User> GetUser(int id);
 
