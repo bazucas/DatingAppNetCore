04a523f48f9424b231743e2e0da75db96f806d87
Luís Ricardo Silva Inácio <luis.inacio@aitec.pt>
2018-07-10 10:54:00 +0100

    tap

--- SPA/src/app/members/member-card/member-card.component.ts
+++ SPA/src/app/members/member-card/member-card.component.ts
@@ -22,7 +22,6 @@ export class MemberCardComponent implements OnInit {
   }
 
   sendLike(id: number) {
-    debugger;
     this.userService.sendLike(this.authService.decodedToken.nameid, id)
       .subscribe(data => {
         this.alertify.success('You have liked: ' + this.user.knownAs);
